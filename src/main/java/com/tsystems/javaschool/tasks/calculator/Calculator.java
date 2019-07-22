f988314281a15b1cb7c296ebabeb59833d1dc4e8
Sergey Khatuntsev <s.khatuntsev@gmail.com>
2019-07-22 20:14:58 +0300

    commit to trigger codeship build

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -5,14 +5,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
 public class Calculator {
-    /**
-     * Evaluate statement represented as string.
-     *
-     * @param statement mathematical statement containing digits, '.' (dot) as decimal mark,
-     *                  parentheses, operations signs '+', '-', '*', '/'<br>
-     *                  Example: <code>(1 + 38) * 4.5 - 1 / 2.</code>
-     * @return string value containing result of evaluation or null if statement is invalid
-     */
+   
     public String evaluate(String statement) {
 
 
